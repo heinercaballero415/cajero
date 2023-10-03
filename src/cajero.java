26de26bb3351fc177893fdce5a36faf41804e53c
jarold123 <110684858+jarold123@users.noreply.github.com>
2023-10-03 16:29:45 -0500

    agrego ciclo white

--- src/cajero.java
+++ src/cajero.java
@@ -2,36 +2,34 @@ import javax.swing.*;
 
 public class cajero {
     public static void main(String[] args) {
+        float ingreso=0,retiro;
+        int item = 0 ;
+         int  saldo = 2000 ;
+float saldoActual = saldo + ingreso;
+while (item != 3 ){
+            item = Integer.parseInt(JOptionPane.showInputDialog(null, "bienvenido al cajero \n" +
+                    "1. ingresar dinero a la cuenta \n" +
+                    "2. retiror de dinero de la cuenta \n" +
+                    "3. salir"));
+            switch (item) {
+                case 1:
+                    ingreso = Integer.parseInt(JOptionPane.showInputDialog("Porfavor ingrese cuanta plata va a guardar"));
+                    saldoActual =  (saldoActual + ingreso);
+                    JOptionPane.showMessageDialog(null, "saldo actual es:" + saldoActual);
+                    break;
+                case 2:
+                    retiro = Integer.parseInt(JOptionPane.showInputDialog("porfavor ingrese cuanto va a retirar"));
+                    if (retiro > saldoActual ) {
+                        JOptionPane.showMessageDialog(null, "saldo insuficiente pobreton ");
+                    } else {
+                        saldoActual = saldoActual - retiro ;
+                        JOptionPane.showMessageDialog(null, "saldo actual:" + saldoActual);
+                    }
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, " a salido del sistema");
 
-        final int saldo_inicial = 2000;
-        int opcion;
-        float ingreso, retiro, saldo_actual;
-
-
-        opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "bienvenido al cajero \n" +
-                "1. ingresar dinero a la cuenta \n" +
-                "2. retiror de dinero de la cuenta \n" +
-                "3. salir"));
-
-        switch (opcion) {
-            case 1:
-                ingreso = Integer.parseInt(JOptionPane.showInputDialog("Porfavor ingrese cuanta plata va a guardar"));
-                saldo_actual = saldo_inicial + ingreso;
-                JOptionPane.showMessageDialog(null, "saldo actual es:" + saldo_actual);
-                break;
-            case 2:
-                retiro = Integer.parseInt(JOptionPane.showInputDialog("porfavor ingrese cuanto va a retirar"));
-                if (retiro > saldo_inicial) {
-                    JOptionPane.showMessageDialog(null, "saldo insuficiente pobreton ");
-                } else {
-                    saldo_actual = saldo_inicial - retiro;
-                    JOptionPane.showMessageDialog(null, "saldo actual:" + saldo_actual);
-                }
-                break;
-            case 3:
-                JOptionPane.showMessageDialog(null, " a salido del sistema");
-
+            }
         }
     }
 }
-
